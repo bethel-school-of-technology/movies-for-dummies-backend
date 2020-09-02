264439ce7ee0291b25f21c48a1a1e2e216874e92
Spencer Theriault <spencertheriault@yahoo.com>
2020-09-01 20:11:15 -0400

    Front End Back End Integration

--- moviesfordummies/routes/index.js
+++ moviesfordummies/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const mysql = require('mysql');
+var staticModels = require('../staticModels/planets');
 
 var connection = mysql.createConnection({
   host: '127.0.0.1',
@@ -27,6 +28,13 @@ connection.query(query, (err, results) => {
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/staticPlanets', function (req, res, next) {
+
+  res.send(JSON.stringify(
+    staticModels.planet
+  ));
 });
 
 module.exports = router;
