4182d0d144bbc177e4c93ba7451894fb5ad1b86e
Spencer Theriault <spencertheriault@yahoo.com>
2020-09-26 22:05:38 -0400

    database connection

--- moviesfordummies/routes/index.js
+++ moviesfordummies/routes/index.js
@@ -5,27 +5,6 @@ const models = require('../models');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Password1!',
-  database: 'movieUser'
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error(err.message);
-    return;
-  }
-  console.log('Yay! You are connected to the database!');
-});
-
-const query = `SELECT * from user LIMIT 10`;
-
-connection.query(query, (err, results) => {
-  if (err) throw err;
-  console.log(results);
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
