d8b2293d1a3ba9649f66289a22c74b111a2394bc
Spencer Theriault <spencertheriault@yahoo.com>
2020-10-01 18:45:28 -0400

    CORS addition

--- moviesfordummies/app.js
+++ moviesfordummies/app.js
@@ -10,7 +10,20 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Cors setup
+
+var cors = require('cors')
+app.use(cors())
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+ 
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
